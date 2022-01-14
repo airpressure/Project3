f29c69bcbaae5dd753968f25360121cdf4669065
STD <ei0407av@aol.com>
2022-01-14 11:49:25 +0200

    Added myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Hey man"
+A="This is a line"
+echo "Result is $A"
+echo 'Result is $A'
+echo "This will expand: $A.  This will NOT expand: \$A"
+
+#Making arithmetics
+
+p1="30"
+p2=4
+p=$(($p1 + $p2))
+echo $p
